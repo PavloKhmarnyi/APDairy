199f97187107965296a87000b6c3a8c875bb8e0f
Pavlo <icomm98@gmail.com>
2016-06-08 22:35:46 +0300

    base projects core

---
+++ AndroidStudioProjects/APDairy2.0/app/src/main/java/com/example/pavlo/apdairy20/utilities/Config.java
@@ -0,0 +1,10 @@
+package com.example.pavlo.apdairy20.utilities;
+
+/**
+ * Created by pavlo on 08.06.16.
+ */
+public class Config {
+
+    public static final String NASA_APOD_URL = "https://api.nasa.gov/planetary/apod";
+    public static final String API_KEY = "TKxEhm2HhrQUL3vJ0CblUPEP0aXBOPCRsLFIVeWO";
+}
